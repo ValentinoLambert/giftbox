8fa51ab1ec3819d9f3ba11d991040bd7b1f6bf1d
noe franoux <noe.franoux7@etu.univ-lorraine.fr>
2025-05-13 16:29:21 +0200

    ajout prestations

---
+++ giftbox.squelette/gift.appli/src/console/list_prestations.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use gift\appli\utils\Eloquent;
+use gift\appli\models\Prestation;
+
+Eloquent::init(__DIR__ . '/../conf/gift.db.conf.ini');
+
+$prestations = Prestation::all();
+
+foreach ($prestations as $prestation) {
+    echo "Libellé: {$prestation->libelle}\n";
+    echo "Description: {$prestation->description}\n";
+    echo "Tarif: {$prestation->tarif}\n";
+    echo "Unité: {$prestation->unite}\n";
+    echo "-------------------------\n";
+}
---
+++ giftbox.squelette/gift.appli/src/models/Prestation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace gift\appli\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Prestation extends Model
+{
+    protected $table = 'prestation';
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    public $timestamps = false;
+    protected $fillable = ['libelle', 'description', 'tarif', 'unite', 'url', 'img', 'cat_id'];
+}
