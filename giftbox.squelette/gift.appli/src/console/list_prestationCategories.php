3294d92845e1f5aa90bdd33fa81e57d57657a3a7
e7160u <valentino.lambert94@gmail.com>
2025-05-13 17:27:08 +0200

    question 4

---
+++ giftbox.squelette/gift.appli/src/console/list_prestationCategories.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use gift\appli\utils\Eloquent;
+use gift\appli\models\Prestation;
+
+Eloquent::init(__DIR__ . '/../conf/gift.db.conf.ini');
+
+/*$
+V1 :
+prestations = Prestation::all();
+
+foreach ($prestations as $prestation) {
+    echo "Libellé prestation: {$prestation->libelle}\n";
+    echo "Catégorie: {$prestation->categorie->libelle}\n";
+    echo "Tarif: {$prestation->tarif}\n";
+    echo "Unité: {$prestation->unite}\n";
+    echo "\n";
+}
+*/
+//V2 :
+$prestations = Prestation::with('categorie')->get();
+
+foreach ($prestations as $prestation) {
+    echo "Libellé prestation: {$prestation->libelle}\n";
+    echo "Catégorie: {$prestation->categorie->libelle}\n";
+    echo "Tarif: {$prestation->tarif}\n";
+    echo "Unité: {$prestation->unite}\n";
+    echo "\n";
+}
--- giftbox.squelette/gift.appli/src/models/Prestation.php
+++ giftbox.squelette/gift.appli/src/models/Prestation.php
@@ -4,12 +4,12 @@ namespace gift\appli\models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Prestation extends Model
-{
+class Prestation extends Model {
     protected $table = 'prestation';
     protected $primaryKey = 'id';
-    public $incrementing = false;
-    protected $keyType = 'string';
     public $timestamps = false;
-    protected $fillable = ['libelle', 'description', 'tarif', 'unite', 'url', 'img', 'cat_id'];
+
+    public function categorie() {
+        return $this->belongsTo(Categorie::class, 'cat_id');
+    }
 }
