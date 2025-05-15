34e6324a402474f7eef9be431395e446f014c148
noe franoux <noe.franoux7@etu.univ-lorraine.fr>
2025-05-15 08:18:51 +0200

    correction controlers ça marche normalement

--- giftbox.squelette/gift.appli/src/controlers/GetCategorieByIdAction.php
+++ giftbox.squelette/gift.appli/src/controlers/GetCategorieByIdAction.php
@@ -2,16 +2,26 @@
 
 namespace gift\appli\controlers;
 
+use gift\appli\models\Categorie;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpNotFoundException;
 
 class GetCategorieByIdAction extends AbstractAction {
     public function __invoke(Request $rq, Response $rs, array $args): Response {
-        $id = $args['id'];
-        $rs->getBody()->write(<<<HTML
-            <h1>Détail de la catégorie $id</h1>
-            <p>Informations statiques pour la catégorie $id.</p>
-        HTML);
-        return $rs->withHeader('Content-Type', 'text/html')->withStatus(200);
+        $id = $args['id'] ?? null;
+
+        if ($id === null) {
+            throw new HttpBadRequestException($rq, 'ID de catégorie manquant');
+        }
+
+        $cat = Categorie::find($id);
+        if (!$cat) {
+            throw new HttpNotFoundException($rq, 'Catégorie non trouvée');
+        }
+
+        $rs->getBody()->write(json_encode($cat));
+        return $rs->withHeader('Content-Type', 'application/json');
     }
 }
--- giftbox.squelette/gift.appli/src/controlers/GetPrestationAction.php
+++ giftbox.squelette/gift.appli/src/controlers/GetPrestationAction.php
@@ -2,22 +2,26 @@
 
 namespace gift\appli\controlers;
 
+use gift\appli\models\Prestation;
 use Slim\Exception\HttpBadRequestException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpNotFoundException;
 
 class GetPrestationAction extends AbstractAction {
     public function __invoke(Request $rq, Response $rs, array $args): Response {
         $id = $rq->getQueryParams()['id'] ?? null;
 
         if (is_null($id)) {
-            throw new HttpBadRequestException($rq, "Paramètre 'id' manquant dans l'URL.");
+            throw new HttpBadRequestException($rq, 'ID de prestation manquant');
         }
 
-        $rs->getBody()->write(<<<HTML
-            <h1>Détail de la prestation $id</h1>
-            <p>Contenu fictif pour la prestation avec ID = $id.</p>
-        HTML);
-        return $rs->withHeader('Content-Type', 'text/html')->withStatus(200);
+        $prestation = Prestation::find($id);
+        if (!$prestation) {
+            throw new HttpNotFoundException($rq, 'Prestation non trouvée');
+        }
+
+        $rs->getBody()->write(json_encode($prestation));
+        return $rs->withHeader('Content-Type', 'application/json');
     }
 }
