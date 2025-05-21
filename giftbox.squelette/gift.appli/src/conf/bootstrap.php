c64a1701fdd12c7b5d56dc1e6e2670dccb81a47c
e7160u <valentino.lambert94@gmail.com>
2025-05-21 15:02:37 +0200

    td4 ex1

--- giftbox.squelette/gift.appli/src/conf/bootstrap.php
+++ giftbox.squelette/gift.appli/src/conf/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 use gift\appli\utils\Eloquent;
 
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -11,6 +13,10 @@ $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, false, false);
 
 Eloquent::init(__DIR__ . '/gift.db.conf.ini');
+
+$twig = Twig::create(__DIR__ . '/../views', ['cache' => false]);
+
+$app->add(TwigMiddleware::create($app, $twig));
 
 (require_once __DIR__ . '/routes.php')($app);
 
