246d99657c1cb619f769788a44794f56c067a582
e7160u <valentino.lambert94@gmail.com>
2025-05-21 16:57:25 +0200

    td4 ex 2 ( fin )

--- giftbox.squelette/gift.appli/src/controlers/GetCategoriesAction.php
+++ giftbox.squelette/gift.appli/src/controlers/GetCategoriesAction.php
@@ -2,19 +2,18 @@
 
 namespace gift\appli\controlers;
 
+use gift\appli\models\Categorie;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
 
 class GetCategoriesAction extends AbstractAction {
     public function __invoke(Request $rq, Response $rs, array $args): Response {
-        $rs->getBody()->write(<<<HTML
-            <h1>Liste des catégories</h1>
-            <ul>
-                <li><a href="/categorie/1">Catégorie 1</a></li>
-                <li><a href="/categorie/2">Catégorie 2</a></li>
-                <li><a href="/categorie/3">Catégorie 3</a></li>
-            </ul>
-        HTML);
-        return $rs->withHeader('Content-Type', 'text/html')->withStatus(200);
+
+        $categories = Categorie::all();
+
+        $view = Twig::fromRequest($rq);
+
+        return $view->render($rs, 'listCategories.twig', ['categories' => $categories]);
     }
 }
