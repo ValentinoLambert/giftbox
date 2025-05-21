6296f727efaeb9546eefe472ec7b2c1cf6e511ef
noe franoux <noe.franoux7@etu.univ-lorraine.fr>
2025-05-21 17:22:02 +0200

    ajout routes de bases

--- giftbox.squelette/gift.appli/public/index.php
+++ giftbox.squelette/gift.appli/public/index.php
@@ -1,9 +1,13 @@
 <?php
 declare(strict_types=1);
 
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
 require_once __DIR__ . '/../src/vendor/autoload.php';
 
 
 $app = require_once __DIR__ . '/../src/conf/bootstrap.php';
+
 
 $app->run();
--- giftbox.squelette/gift.appli/src/conf/routes.php
+++ giftbox.squelette/gift.appli/src/conf/routes.php
@@ -2,14 +2,16 @@
 declare(strict_types=1);
 
 use Slim\App;
+use gift\appli\controlers\HomeAction;
 use gift\appli\controlers\GetCategoriesAction;
 use gift\appli\controlers\GetCategorieByIdAction;
 use gift\appli\controlers\GetPrestationAction;
 
 return function (App $app) : App {
-    $app->get('/categories', GetCategoriesAction::class)->setName('categories');
-    $app->get('/categorie/{id}', GetCategorieByIdAction::class)->setName('categorie');
-    $app->get('/prestation', GetPrestationAction::class)->setName('prestation');
+    $app->get('/', HomeAction::class);
+    $app->get('/categories', GetCategoriesAction::class);
+    $app->get('/categorie/{id}', GetCategorieByIdAction::class);
+    $app->get('/prestation', GetPrestationAction::class);
 
     return $app;
 };
